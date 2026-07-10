09e3ecf7ed4419f4fb95afe49ad744316789aab9
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-07-10 12:09:51 +0500

    String variables

---
+++ JavaScript/Class-02/app.js
@@ -0,0 +1,27 @@
+console.log("Hello External Js File");
+
+// Let's how to create string variables :)
+
+// let myname = "My name is Hassan, Im 21 years old ";
+// let fruit = "Banana";
+// let animal = "Cow";
+// console.log(fruit);
+// console.log(myname);
+// console.log(animal);
+// console.log(myname, fruit, animal);
+
+
+// Now how i changed the value of my variable
+
+// let myname = "Hassan";
+// console.log(myname);
+
+// myname = "muteeb";
+// console.log(myname);
+
+
+// now i only write variable name and next line i defined what is this value
+
+let nationality;
+nationality = "America"
+console.log(nationality);
