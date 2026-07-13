58abfb94c00736c7a3488dbaa2ca0e589f3df71e
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-07-13 12:33:05 +0500

    covered chapter 3, 4 & 5

--- JavaScript/Class-03/app.js
+++ JavaScript/Class-03/app.js
@@ -84,3 +84,12 @@
 
 // const popularNumber = 15 % 5;      // modulus
 // console.log(popularNumber);
+
+// Practice
+let num1 = 12;
+let num2 = 4;
+console.log(num1 + num2); // addition
+console.log(num1 - num2); // subtraction
+console.log(num1 * num2); // multiplication
+console.log(num1 / num2); // division
+console.log(num1 % num2); // modulus
