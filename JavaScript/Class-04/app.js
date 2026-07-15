d02580aad6cf5c3486dccbfe22d253240342b8dc
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-07-15 12:34:29 +0500

    Covered chapter 06, 07

---
+++ JavaScript/Class-04/app.js
@@ -0,0 +1,72 @@
+// Class-03 Revision!
+
+// let myName = "Hassan"
+// console.log(myName);
+
+// const useYourName = "Bilal";
+// // useYourName = "Kashif";
+// console.log(useYourName);
+
+// let doubleUsage = "Hello";
+// doubleUsage = "World";
+// console.log(doubleUsage);
+
+// doubleUsage  ====> camelCase!
+
+// Class-04 Start: Unfamiliar operators
+// POST INCREMENT
+// let num = 1;
+// let newNum = num++;
+// console.log(newNum);
+// console.log(num);
+// console.log(num);
+
+// POST DECREMENT
+// let num = 2;
+// let newNum = num--;
+// console.log(newNum);
+// console.log(num);
+
+// PRE INCREMENT
+// let num = 1;
+// let newNum = ++num;
+// console.log(newNum);
+// console.log(num);
+
+// PRE DECREMENT
+// let num = 2;
+// let newNum = num--;
+// console.log(newNum);
+// console.log(num);
+
+// Chapter 07: Eliminating ambiguity
+
+// let totalCost = 1 + 3 *  4;
+// console.log(totalCost);
+
+// let totalCost =  8 + (4 * 3);
+// console.log(totalCost);
+
+// let totalCost2 =  2 * (4 + 3);
+// console.log(totalCost2);
+
+// let resultOfComputation = (2 * 4) * 4 + 2;
+// console.log(resultOfComputation);
+
+// PRACTICE TIME :
+// Question:01
+// answer predictions Q1:  69, 20, 10, 48, 49, 50, 19, 70
+ 
+// let addThings = 20;
+// let secondThing = 10;
+// let addMoreThings = addThings++ + addThings-- + --addThings + secondThing - 20;
+// console.log(addMoreThings);
+// console.log(addThings++);
+// console.log(addThings);
+
+// Question:02 
+// let division =  20 / 2;
+// let multiplication = 10 * 4;
+// let addBothThings = division + multiplication + division++;
+// console.log(addBothThings);
+// console.log(division);
