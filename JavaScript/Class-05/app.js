9d61c9dff41f28be54b3509d0275fcf063f87389
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-07-17 11:52:20 +0500

    Covered chapter: 08 & 09

---
+++ JavaScript/Class-05/app.js
@@ -0,0 +1,46 @@
+// Revision Class-04
+
+// Question 1
+// let num = 10;
+// console.log(num);
+// console.log(num++);
+// console.log(num);
+
+// Question 2
+// console.log((10 + 5) * 2);
+
+// Question 3
+// let age = 20;
+// console.log(age + 1);
+// console.log(age + 1);
+// console.log(++age);
+// console.log(age);
+
+
+// START NEW CLASS
+// Chapter: 08 ====> String Concetenation :)
+
+// let firstName = "Muhammad";
+// let lastName = "Hassan";
+// let fullName = firstName + " " + lastName;
+// console.log(firstName);
+// console.log(lastName);
+// console.log(fullName);
+
+// let city = "Karachi";
+// console.log("City:", city);
+
+// let age = 21;
+// console.log("Age:", age);
+
+// Chapter: 09 ===>  Prompt!
+
+let userName = prompt("Please enter your name")
+let userAge = prompt("Please enter your age");
+let userHobbies = prompt("Enter your Hobbies here");
+// alert(userName)
+// console.log(userName);
+// console.log(userAge);
+console.log("Username:", userName);
+console.log("Userage:", userAge);
+console.log("UserHobbies:", userHobbies);
