2866e5113086f2946bc182246cd80ab7c810b7b0
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-07-22 13:18:48 +0500

    if statement & comparison operators

---
+++ JavaScript/Class-06/app.js
@@ -0,0 +1,72 @@
+// Chapter 09: Prompts revision
+
+// let userName = prompt("What is your age?");
+// alert(userName);
+// console.log(userName);
+
+// let firstName = "Hassan";
+// let lastName = "Owais";
+// // console.log(firstName + " " + lastName);
+// let fullName = firstName + " " + lastName;
+// // console.log(fullName);
+
+// let userCountry = prompt("Enter your country Name");
+// let userCity = prompt("Enter your city Name"); 
+// // let userInfo = userCountry + userCity;
+// console.log("UserCountry" + " "  + userCountry);
+// console.log("UserCity" + " " + userCity);
+
+// Chapter: 11
+// Comparison Operators
+
+// console.log(10 > 5);
+// console.log("10 > 5");
+// console.log(10 < 20);
+// console.log(10 >= 20); // false
+// console.log(10 <= 20);  //  true
+// console.log(18 >= 20);
+// console.log(18 >= 18);
+// console.log(10 == 30);
+// console.log(10 == 10);
+// console.log(30 != 20);
+// console.log(30 !== 20);  // ! hamare pass false ke liye use hota hai
+// console.log("triple equal to ====>", 10 === 10);  // true
+// console.log("triple equal to with string====>", 10 === "10"); // false 08 students || true 13 students
+// console.log("double equal to with string====>", 10 == "10");  // half half student baqi beech ke hen nh jawab de rahe!
+
+// Triple equal to check it dataType or the value.
+// Double equal to doestnot check the dataType only check the value!
+
+// Chapter 10: If statement!
+
+// let userCountry = prompt("Add your Country Name");
+// console.log(userCountry);
+
+// if(userCountry === "pakistan"){
+//     alert("Mubarak ho bhai me bh isi country ka ho!!");
+// };
+
+// if(userCountry != "pakistan"){
+//     alert("Scammers se bach gay, tumhare sath daketi bh nh hui! are you saved")
+// };
+
+// let myAge = 20;
+// let userAge = 30;
+// if(myAge <= userAge){
+//     alert("Userage is greater then is your age!")
+// }
+
+// let userAge = prompt("Enter your Age");
+// if(userAge >= 18){
+//     alert("You are eligible to visit the website!")
+// };
+
+// let result = prompt("Enter your percentage");
+// if(result >= 50){
+//     alert("50 percentage is valid!")
+// }
+
+// let cast = "memon";
+// if(cast === "memon"){
+//     console.log("Yes, Im memon!");
+// }        
